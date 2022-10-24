f153763d5e608d73c5d57823e7e88ab48614558d
xuqiqi7 <1367440657@qq.com>
2022-10-24 22:33:53 +0800

    router修改

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
 
     <el-row>
       <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
+      <el-button type="primary">主要按钮1</el-button>
+      <el-button type="success">成功按钮1</el-button>
+      <el-button type="info">信息按钮1</el-button>
+      <el-button type="warning">警告按钮1</el-button>
+      <el-button type="danger">危险按钮1</el-button>
     </el-row>
     <el-row>
       <el-button icon="el-icon-search" circle></el-button>
